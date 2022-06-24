f33fcd0378728ea986d608f884c2cd4d29586426
betrider <45031487+betrider@users.noreply.github.com>
2022-06-24 16:51:36 +0900

    Update 42-trapping-rain-water.py

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -18,3 +18,26 @@ class Solution:
           result += min(toLeft[i], toRight[i]) - height[i];
         
         return result;
+    
+# def trap(height: [int]) -> int:
+#     maxLength = len(height) - 1;
+
+#     result = 0;
+
+#     toRight = [0 for i in range(maxLength + 1)];
+#     toLeft = [0 for i in range(maxLength + 1)];
+
+#     toRight[0] = height[0];
+#     toLeft[maxLength] = height[maxLength]
+
+#     for i in range(1, len(height)): 
+#       toRight[i] = max(height[i], toRight[i-1]);
+#       toLeft[maxLength-i] =  max(height[maxLength-i], toLeft[maxLength+1-i]);
+
+#     for i in range(0, maxLength + 1):
+#       result += min(toLeft[i], toRight[i]) - height[i];
+    
+#     return result;
+    
+# height = [0,1,0,2,1,0,1,3,2,1,2,1];
+# trap(height);
